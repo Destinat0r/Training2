8aed64e2dff474b8e6f71b4c08eda877348263ce
Destinat0r <ashavrn@protonmail.com>
2019-12-16 06:39:30 +0200

    added getters and setters

--- SpringEntityDemo/src/main/java/com/companic/entity/User.java
+++ SpringEntityDemo/src/main/java/com/companic/entity/User.java
@@ -14,6 +14,29 @@ public class User {
     @Value("${user.last_name}")
     private String lastName;
 
-    
+    public User() {}
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
