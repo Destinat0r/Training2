5d54a9021d32c667fc74c7db96460ced8a980b7b
Destinat0r <ashavrn@protonmail.com>
2019-12-16 06:30:04 +0200

    added confing, properties, created User

--- SpringEntityDemo/src/main/java/com/companic/config/Conguration.java
+++ SpringEntityDemo/src/main/java/com/companic/config/Conguration.java
@@ -1,8 +1,13 @@
 package com.companic.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan("com.companic")
+@PropertySource("classpath:user.properties")
+
 public class Conguration {
 
 }
---
+++ SpringEntityDemo/src/main/java/com/companic/entity/User.java
@@ -0,0 +1,19 @@
+package com.companic.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    @Value("${user.id}")
+    private int id;
+
+    @Value("${user.first_name}")
+    private String firstName;
+
+    @Value("${user.last_name}")
+    private String lastName;
+
+
+
+}
