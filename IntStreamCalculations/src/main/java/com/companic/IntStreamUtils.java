f2710d91f757c9d65debb063d64666f4b6c58f1a
Destinat0r <ashavrn@protonmail.com>
2019-12-17 18:13:30 +0200

    added getAverage()

--- IntStreamCalculations/src/main/java/com/companic/IntStreamUtils.java
+++ IntStreamCalculations/src/main/java/com/companic/IntStreamUtils.java
@@ -7,4 +7,9 @@ public class IntStreamUtils {
     public static int getAverageUsingAverage(int[] intArray) {
         return (int) Math.round(Arrays.stream(intArray).average().getAsDouble());
     }
+
+    public static int getAverage(int[] intArray) {
+        return (int) Math.round(Arrays.stream(intArray).sum() / (double) intArray.length);
+
+    }
 }
