36c268c91e56065fb68ab552ef9fd06605ca0a65
Destinat0r <ashavrn@protonmail.com>
2019-12-17 17:33:55 +0200

    added another array to shouldReturnAverageUsingAverage(), added shouldReturnAverageUsingCalculation()

--- IntStreamCalculations/src/test/java/IntStreamTest.java
+++ IntStreamCalculations/src/test/java/IntStreamTest.java
@@ -9,6 +9,17 @@ public class IntStreamTest {
     public void shouldReturnAverageUsingAverage() {
 
         int[] intArray = {5, 3, 9, 7};
+        int[] intArray2 = {4, 5, 6, 2, 12, 5, 3, 8};
         assertEquals("should return 6", 6,  IntStreamUtils.getAverageUsingAverage(intArray));
+        assertEquals("should return 6", 6,  IntStreamUtils.getAverageUsingAverage(intArray2));
     }
+
+    public void shouldReturnAverageUsingCalculation() {
+        int[] intArray = {5, 3, 9, 7};
+        int[] intArray2 = {4, 5, 6, 2, 12, 5, 3, 8};
+        assertEquals("should return 6", 6,  IntStreamUtils.getAverage(intArray));
+        assertEquals("should return 6", 6,  IntStreamUtils.getAverage(intArray2));
+    }
+
+
 }
