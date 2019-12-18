9faaa72d6476509d4b0da546fc36cea6b9edcbaf
Destinat0r <ashavrn@protonmail.com>
2019-12-18 13:35:15 +0200

    added getMinimalAndIndex(), added nullcheck to all methods, some vars renamed.

--- IntStreamCalculations/src/main/java/com/companic/IntStreamUtils.java
+++ IntStreamCalculations/src/main/java/com/companic/IntStreamUtils.java
@@ -1,23 +1,41 @@
 package com.companic;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class IntStreamUtils {
 
-    public static int getAverageUsingAverage(int[] intArray) {
-        return (int) Math.round(Arrays.stream(intArray).average().getAsDouble());
+    public static int getAverageUsingAverage(int[] arr) {
+        Objects.requireNonNull(arr, "arr is null");
+        return (int) Math.round(Arrays.stream(arr).average().getAsDouble());
     }
 
-    public static int getAverage(int[] intArray) {
-        return (int) Math.round(Arrays.stream(intArray).sum() / (double) intArray.length);
+    public static int getAverage(int[] arr) {
+        Objects.requireNonNull(arr, "array is null");
+        return (int) Math.round(Arrays.stream(arr).sum() / (double) arr.length);
 
     }
 
-    public static int countZeros(int[] array) {
-        return (int) Arrays.stream(array).filter(x -> x == 0).count();
+    public static int countZeros(int[] arr) {
+        Objects.requireNonNull(arr, "array is null");
+        return (int) Arrays.stream(arr).filter(x -> x == 0).count();
     }
 
-    public static int countPositives(int[] array) {
-        return (int) Arrays.stream(array).filter(x -> x > 0).count();
+    public static int countPositives(int[] arr) {
+        Objects.requireNonNull(arr, "array is null");
+        return (int) Arrays.stream(arr).filter(x -> x > 0).count();
+    }
+
+    public static int[] getMinimalAndIndex(int[] arr) {
+        Objects.requireNonNull(arr, "array is null");
+
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        return IntStream.range(0, arr.length)
+                .mapToObj(n -> new int[]{arr[n], n})
+                .filter(elem -> elem[0] == min)
+                .findFirst()
+                .get();
     }
 }
